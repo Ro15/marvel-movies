1a1edd2ab09c9b2b77690ae1bda19d02c9cd6755
Ro15 <rohithreddyravi4@gmail.com>
2023-11-21 16:33:50 -0500

    Working till now intial page

--- src/App.js
+++ src/App.js
@@ -1,37 +1,45 @@
+import './componets/category-item/category-item.styles.scss';
+import CategoryItem from "./componets/category-item/category-item.component";
+import avengerImage from "./assets/avenger.jpg";
+import TitanImage from "./assets/titan.jpg";
+import dcImage from "./assets/dc.jpg";
+import balayyaImage from "./assets/balayya.jpg";
+import HerosImage from "./assets/Heros.jpg";
 const App = () => {
   const categories = [
     {
       id: 1,
       title: "Avengers",
+      imageUrl:avengerImage,
     },
     {
       id: 2,
       title: "Attack on Titans",
+      imageUrl:TitanImage,
     },
     {
       id: 3,
       title: "DC herons",
+      imageUrl:dcImage,
     },
     {
       id: 4,
       title: "Real Characters",
+      imageUrl:balayyaImage,
     },
     {
       id: 5,
       title: "Intlo Heros",
+      imageUrl:HerosImage,
     },
   ];
-  return (
+  return(
     <div className="categories-containers">
-      {categories.map(({ title,id }) => (
-        <div key={id}className="category-container">
-          {/* <img/>> */}
-          <div className="category-body-container">
-            <h2>{title}</h2>
-            <p>Who Are You?</p>
-          </div>
-        </div>
+      {categories.map((category)=>(
+        <CategoryItem key={category.id} category={category}/>
+      
       ))}
+    
     </div>
   );
 };
